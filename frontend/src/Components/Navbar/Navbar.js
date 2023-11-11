d4c5a8b96f31ad3935fdfdd42566487c23bce91a
Keyvan <keyvan.nikazm@gmail.com>
2023-11-11 18:31:08 +0100

    change navbar and add new page.js

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -30,9 +30,6 @@ const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-            </li>
-            <li class="nav-item">
               <a class="nav-link" href="#" data-uri="/aboutUs">About</a>
             </li>
             <li class="nav-item">
--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,12 +1,14 @@
 import aboutUs from '../Pages/aboutUs';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import creationQuizz from '../Pages/creationQuizz';
 
 const routes = {
   "/": HomePage,
   '/aboutUs': aboutUs,
   '/login': NewPage,
-  '/register' : NewPage
+  '/register' : NewPage,
+  '/creationQuizz' : creationQuizz
 };
 
 export default routes;
