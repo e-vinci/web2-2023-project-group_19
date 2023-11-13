66ff9adfb107f644f0b4f63cee9078bd8c5abfe4
Keyvan <keyvan.nikazm@gmail.com>
2023-11-13 13:47:50 +0100

    new page with html dynamic

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -29,6 +29,9 @@ const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/creationQuizz">(création)</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="#" data-uri="/aboutUs">About</a>
             </li>
             <li class="nav-item">
