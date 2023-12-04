980ecfdea128fc87912a36b59e786cc528ce6d53
LukenV <alexis.arnaud@student.vinci.be>
2023-12-05 00:49:12 +0100

    edit : useless origins for cors options

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'http://localhost:3000', 'https://oryx-cdn.microsoft.io:443'],
+  origin: ['http://localhost:8080'],
 };
 
 const usersRouter = require('./routes/users');
@@ -14,12 +14,12 @@ const creatQuizzRouter = require('./routes/creationQuizz');
 
 const app = express();
 
+app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(cors(corsOptions));
 
 app.use('/users', usersRouter);
 app.use('/quizzes', quizzesRouter);
