26ec947b38b8a879950e0c47746b623352c55e47
LukenV <alexis.arnaud@student.vinci.be>
2023-12-09 16:37:01 +0100

    edit : confidentialite page layout

--- frontend/src/Components/Pages/ConfidentialitePage.js
+++ frontend/src/Components/Pages/ConfidentialitePage.js
@@ -1,58 +1,56 @@
 const Confidentialité = () => {
+    const siteUrl = 'https://lukenv.github.io/quizzler/';
     const main = document.querySelector('main');
     main.innerHTML = `
-    <div id="inner_content-7-239" class="ct-inner-content">
-    <p class="has-text-align-right"><span style="text-decoration: underline;">Date de mise à jour : 21 novembre 2023</span></p>
-    <p class="has-text-align-center"><strong>Accès rapide :</strong></p>
-    <p class="has-text-align-center"><a href="#compte" data-type="internal" data-id="#compte">1.Création d'un compte</a><br><a href="#cookies" data-type="internal" data-id="#cookies">2. Cookies</a></p>
-    <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
-    <p>Cette politique vous informe sur les types d’utilisation qui sont fait de vos données et sur la façon dont nous les protégeons lorsque vous utilisez notre site internet www.culturequizz.com. Il appartient au visiteur du site de rester informé de ces évolutions qui l’engagent en consultant la présente page avant toute utilisation du présent site internet.</p>
-    <span id="ezoic-pub-ad-placeholder-102" data-inserter-version="2"></span>
-    <span class="ezoic-ad ezoic-at-0 box-3 box-3102 adtester-container adtester-container-102" data-ez-name="culturequizz_com-box-3">
-        <span id="div-gpt-ad-culturequizz_com-box-3-0" ezaw="336" ezah="280" style="position:relative;z-index:0;display:inline-block;padding:0;position:sticky !important;top:0px !important;min-height:280px;min-width:336px;" class="ezoic-ad ezads-sticky-intradiv ezoic-adl">
-            <script data-ezscrex="false" data-cfasync="false" type="text/javascript" style="display:none;">if(typeof ez_ad_units == 'undefined'){ez_ad_units=[];}ez_ad_units.push([[336,280],'culturequizz_com-box-3','ezslot_1',102,'0','0', 'culturequizz_com-box-3-0']);if(typeof __ez_fad_position == 'function'){__ez_fad_position('div-gpt-ad-culturequizz_com-box-3-0');}</script>
-            <div id="google_ads_iframe_/1254144,22420319593/culturequizz_com-box-3_0__container__" style="border: 0pt none; width: 336px; height: 280px;"></div>
-        </span>
-    </span>
-    <p>Le gérant s'engage à respecter les dispositions de la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés modifiées et au Règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 dit "<em>RGPD</em>" et prendre toute précaution nécessaire pour préserver la sécurité des informations nominatives confiées.</p>
-    <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
-    <h2 class="wp-block-heading" id="compte">1. Création d'un compte</h2>
-    <p>Certaines informations personnelles sont collectées sur ce site de façon à nous permettre à répondre à certaines de vos demandes d’informations ou de services. En dehors de ces cas spécifiques, il vous est possible de visiter le présent site internet sans avoir à nous transmettre la moindre information.</p>
-    <span id="ezoic-pub-ad-placeholder-110" data-inserter-version="2" data-ezadblocked="true"></span>
-    <p>Listes des informations collectées selon les scénarios :</p>
-    <div class="wp-block-columns is-layout-flex wp-container-core-columns-layout-1 wp-block-columns-is-layout-flex">
-        <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
-            <p><strong>Création d'un compte classique</strong> :</p>
-            <ol>
-                <li>Nom et prénom</li>
-                <li>Adresse de courrier électronique</li>
-                <li>Pseudonyme</li>
-                <li>Mot de passe</li>
-            </ol>
-        </div>
-        <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
-            <p><strong>Création d'un compte avec Facebook ou Google</strong> :</p>
-            <ol>
-                <li>Nom et prénom</li>
-                <li>Adresse de courrier électronique</li>
-                <li>Photo de profil</li>
-                <li>Préférences linguistiques</li>
-            </ol>
-        </div>
+    <div id="inner_content-7-239">
+    <div style="height:50px"></div>
+    <p><span style="text-decoration: underline;">Date de mise à jour : 9 décembre 2023</span></p>
+    <div style="height:50px"></div>
+    <p><strong>Accès rapide :</strong></p>
+    <div style="height:25px"></div>
+    <p>
+        <a href="#compte">1.Création d'un compte</a><br>
+        <a href="#cookies">2. Cookies</a><br>
+        <a href="#supprimer">3. Supprimer un compte</a>
+    </p>
+    <div style="height:25px"></div>
+    <p>
+        Cette politique vous informe sur les types d’utilisation qui sont fait de vos données et sur la façon dont nous les protégeons lorsque vous utilisez notre site internet ${siteUrl}.<br>
+        Il appartient au visiteur du site de rester informé de ces évolutions qui l’engagent en consultant la présente page avant toute utilisation du présent site internet.
+    </p>
+    <div style="height:50px"></div>
+    <p>
+        Le gérant s'engage à respecter les dispositions de la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés modifiées et au Règlement (UE)<br>
+        2016/679 du Parlement européen et du Conseil du 27 avril 2016 dit "<em>RGPD</em>" et prendre toute précaution nécessaire pour préserver la sécurité des informations nominatives confiées.
+    </p>
+    <div style="height:50px"></div>
+    <h2 id="compte">1. Création d'un compte</h2>
+    <div style="height:50px"></div>
+    <p>
+        Certaines informations personnelles sont collectées sur ce site de façon à nous permettre à répondre à certaines de vos demandes d’informations ou de services.<br>
+        En dehors de ces cas spécifiques, il vous est possible de visiter le présent site internet sans avoir à nous transmettre la moindre information.
+    </p>
+    <div style="height:25px"></div>
+    <p>Listes des informations collectées :</p>
+    <div style="height:25px"></div>
+    <div class="wp-block-column">
+        <ol>
+            <li><strong>Adresse de courrier électronique</strong></li>
+            <li><strong>Pseudonyme</strong></li>
+            <li><strong>Mot de passe</strong></li>
+        </ol>
     </div>
-    <h3 class="wp-block-heading" id="presentation-des-finalites-du-traitement">Présentation des finalités du traitement</h3>
-    <p>CultureQuizz.com utilise ces informations pour le bon fonctionnement de l'espace membres et ne sont pas partagées à des sociétés tierces. Aucune information personnelle n'est envoyée à Google ou Facebook concernant votre utilisation du site dans le cadre d'une connexion au travers du social connect.</p>
+    <div style="height:50px"></div>
+    <h3 id="cookies">2. Cookies</h3>
+    <div style="height:50px"></div>
+    <p>${siteUrl} utilise ces informations pour le bon fonctionnement de l'espace membres et ne sont pas partagées à des sociétés tierces.</p>
     <p>Les utilisateurs sont identifiés sur notre site au travers de leur pseudonyme créé au moment de la création du compte.</p>
-    <h3 class="wp-block-heading" id="commentaires-deposes-sur-notre-site">Commentaires déposés sur notre site</h3>
-    <p>Déposer un commentaire sur notre plateforme est réservé aux membres.</p>
-    <h3 class="wp-block-heading" id="contenu-embarque-depuis-d-autres-sites">Contenu embarqué depuis d’autres sites</h3>
-    <p>Les articles de ce site peuvent inclure des contenus intégrés (par exemple des vidéos, images, articles…). Le contenu intégré depuis d’autres sites se comporte de la même manière que si le visiteur se rendait sur cet autre site.</p>
     <p>Ces sites web pourraient collecter des données sur vous, utiliser des cookies, embarquer des outils de suivis tiers, suivre vos interactions avec ces contenus embarqués si vous disposez d’un compte connecté sur leur site web.</p>
-    <span id="ezoic-pub-ad-placeholder-111" data-inserter-version="2" data-ezadblocked="true"></span>
-    <h3 class="wp-block-heading" id="supprimer-vos-donnees">Supprimer vos données</h3>
-    <p>Vous pouvez à n'importe quel moment décider de supprimer votre compte
-
-    `
+    <div style="height:50px"></div>
+    <h3 id="supprimer">3. Supprimer un compte</h3>
+    <div style="height:50px"></div>
+    <p>Vous pouvez à n'importe quel moment décider de supprimer votre compte</p>
+    <div style="height:50px"></div>`;
 
 }
 
