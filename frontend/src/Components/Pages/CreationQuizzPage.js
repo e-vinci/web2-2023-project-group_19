a3e2113c6b55f70aa8acd5ec9a1278d349cee72f
LukenV <alexis.arnaud@student.vinci.be>
2023-12-09 02:03:21 +0100

    edit : createQuizzPage render

--- frontend/src/Components/Pages/CreationQuizzPage.js
+++ frontend/src/Components/Pages/CreationQuizzPage.js
@@ -32,10 +32,10 @@ function renderForm( currentStepCreation ) {
                     <form id="questionForm">
                         <h5 class="classe-titre"> Creation Quizz </h5>
                         <span> Catégorie de quizz : </span>
-                        <input type="text" id="categorieQuizz" placeholder="histoire/geographie/sciences">
+                        <input type="text" id="categorieQuizz" class="regularInput" placeholder="histoire/geographie/sciences">
                         <span> Niveau de difficulté </span>
-                        <input type="number" min="1" max="3" id="niveauDifQuizz"placeholder="Niveau de difficulté">
-                        <input type="submit" id="boutonQuizz" class="Next-question" Next-question>
+                        <input type="number" min="1" max="3" class="regularInput" id="niveauDifQuizz"placeholder="Niveau de difficulté">
+                        <input type="submit" id="boutonQuizz" class="Next-question regularInput" Next-question>
                     </form>
                 </div>
                 <div class="toggler-container">
@@ -60,23 +60,33 @@ function renderForm( currentStepCreation ) {
                     <form id="questionForm">
                         <h5 class="classe-titre"> Creation Quizz </h5>
                         <span> Question : </span>
-                        <input type="text" id="questionQuizz" placeholder="Intitule de la question">
+                        <input type="text" id="questionQuizz" class="regularInput" placeholder="Intitule de la question">
                         <span> Propositions : </span>
-                        <input type="text" id="proposition1" placeholder="Proposition n°1">
-                        <input type="text"id="proposition2" placeholder="Proposition n°2">
-                        <input type="text" id="proposition3" placeholder="Proposition n°3">
+
+                        <div style="display:inline-flex;width:100%;">
+                        <input type="text" id="proposition1" class="propositionsInput" placeholder="Proposition n°1">
+                        <input type="radio" name="proposition">
+                        </div>
+                        <div style="display:flex;width:100%;">
+                        <input type="text"id="proposition2" class="propositionsInput" placeholder="Proposition n°2">
+                        <input type="radio" name="proposition">
+                        </div>
+                        <div style="display:flex;width:100%;">
+                        <input type="text" id="proposition3" class="propositionsInput" placeholder="Proposition n°3">
+                        <input type="radio" name="proposition">
+                        </div>
+                        
                         <span> Réponse correcte </span>
-                        <input type="text" id="reponseQuizz" placeholder="Réponse correcte">
-                        <input type="submit" id="boutonQuizz" class="Next-question" Next-question>
+                        <input type="text" id="reponseQuizz" class="regularInput" placeholder="Réponse correcte">
+                        <input type="submit" id="boutonQuizz" class="Next-question regularInput" Next-question>
                     </form>
                 </div>
                 <div class="toggler-container">
                     <div class="toggle">
                         <div class="toggle-panel toggle-left">
                             <h4 class=" classe-titre4 ">
-                                Bienvenue sur la page de création d’un quizz
+                                Vous êtes à l'étape ${currentStepCreation}/${numberMaxSteps} de la création du quizz
                             </h4>
-                            <h3 classe=" classe-titre3 ">${currentStepCreation}/${numberMaxSteps} </h3>
                         </div>
                     </div>
                 </div>
@@ -87,13 +97,12 @@ function renderForm( currentStepCreation ) {
 
 }
  function makeReactiveForm( currentStepCreation ) {
-  // eslint-disable-next-line no-cond-assign
 
   const formulaire = document.querySelector('#questionForm');
   const boutonFinish = document.querySelector('#boutonQuizz');
   if( currentStepCreation === 11){
-    boutonFinish.innerHTML = "terminer le quizz";
-  }
+    boutonFinish.innerHTML = "Terminer le quizz";
+  };
   formulaire.addEventListener('submit', onAddQuizz );
 
     /* if(!categorieQuizzId || !question || !proposition1 || !proposition2 || !proposition3 ||!difficultee || !reponse){
@@ -151,7 +160,7 @@ async function onAddQuizz(){
         // eslint-disable-next-line no-console
         console.log(`question : ${question}`);
 
-    }
+    };
     
     sessionStorage.setItem('currentStepCreation', currentStepCreation+1);
 
