7eb072bb534b6013d105b74e50ddd0632cadb739
LukenV <alexis.arnaud@student.vinci.be>
2023-12-12 00:31:01 +0100

    edit : API users router get one user from username

--- api/models/users.js
+++ api/models/users.js
@@ -22,7 +22,7 @@ async function readOneUserFromEmail(email) {
 
 async function readOneUserFromUsername(username) {
   const requestString = `
-    SELECT *
+    SELECT id_user, username, nbr_points
     FROM QUIZZLER.users
     WHERE username = '${username}'
   `;
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,11 +4,19 @@ const router = express.Router();
 
 const {
   getLeaderboard,
+  readOneUserFromUsername,
 } = require('../models/users');
 
 router.get('/leaderboard', async (req, res) => {
   const leaderboard = await getLeaderboard();
   res.json(leaderboard);
+});
+
+router.get('/', async (req, res) => {
+  const username = req?.query?.username;
+  const userFound = await readOneUserFromUsername(username);
+  if (!userFound) return undefined;
+  return res.json(userFound);
 });
 
 module.exports = router;
