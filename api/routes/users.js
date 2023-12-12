e4d2d39d13fb80f35ae71ddbab9ac657a961536b
LukenV <alexis.arnaud@student.vinci.be>
2023-12-12 16:16:15 +0100

    edit : API update user points

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const {
   getLeaderboard,
   readOneUserFromUsername,
+  updateUserPoints,
 } = require('../models/users');
 
 router.get('/leaderboard', async (req, res) => {
@@ -18,6 +19,14 @@ router.get('/', async (req, res) => {
   const userFound = await readOneUserFromUsername(username);
   if (!userFound) return undefined;
   return res.json(userFound);
+});
+
+router.post('/updateUserPoints', async (req, res) => {
+  const countPointsToAdd = req?.body?.countPointsToAdd ? req.body.countPointsToAdd : undefined;
+  const userId = req?.body?.userId ? req.body.userId : undefined;
+  if (!countPointsToAdd || !userId) return res.sendStatus(400);
+  const currentCountPoints = await updateUserPoints(userId, countPointsToAdd);
+  return res.json(currentCountPoints);
 });
 
 module.exports = router;
