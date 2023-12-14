9674afdcd9f2fed27ebf87f61266f467cbc22854
LukenV <alexis.arnaud@student.vinci.be>
2023-12-14 18:41:17 +0100

    edit : script bug fix and correction of one answer

--- dataBase/scriptCreatDataBaseProjectWebBac2.sql
+++ dataBase/scriptCreatDataBaseProjectWebBac2.sql
@@ -898,7 +898,7 @@ insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Consta
 insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Trajan',false,90);
 
 -- Quizz 10 :
-insert into QUIZZLER.quizzes(difficultee, categorie, points_rapportes) VALUES (1,'geographie',3);
+insert into QUIZZLER.quizzes(difficultee, categorie, points_rapportes) VALUES (1,'geographie',1);
 -- question 1 quizz 2:
 insert into QUIZZLER.questions(quizz, numero, intitule) VALUES (10,1,'Quel est le plus grand océan de la planète ?');
 -- question 2 quizz 2:
@@ -985,9 +985,9 @@ insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Océan
 insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Océan Pacifique',false,99);
 -- proposition question 10
 --proposition 1 question 10
-insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Inde',false,100);
+insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Inde',true,100);
 --proposition 2 question 10
-insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Chine',true,100);
+insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('Chine',false,100);
 --proposition 3 question 10
 insert into QUIZZLER.propositions(intitule, isreponse, question) VALUES ('États-Unis',false,100);
 
