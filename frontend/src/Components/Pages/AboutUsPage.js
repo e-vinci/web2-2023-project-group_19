96de1a3aeb42290f287233e134e8cbd65793e8fc
LukenV <alexis.arnaud@student.vinci.be>
2023-12-14 01:20:37 +0100

    edit : aboutUsPage style

--- frontend/src/Components/Pages/AboutUsPage.js
+++ frontend/src/Components/Pages/AboutUsPage.js
@@ -12,12 +12,16 @@ const aboutUs = () => {
   <img src="${imageNico}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Nicolas Venturini</h5>
-    <button id="showStory"> description </button>
+    <div class="text-center">
+    <button id="showStory" class="btn btn-info AboutUsPage-description-button"> Description </button>
+    </div>
         <dialog id="Story">
           <form method="dialog">
             <p>Nicolas est le responsable de la base de données. Il a conceptualisé la base de donnée, l'a remplie et l'a déployée avec Simon. Nicolas a également fait le backend de la page du classement et a participé au backend pour la création des quizzes.
             </p>
-              <button id="confirmBtn">Close</button>
+            <div class="text-center">
+            <button id="confirmBtn" class="btn btn-dark" style="font-family:sans-serif">Close</button>
+          </div>
           </form>
         </dialog>
   </div>
@@ -26,13 +30,17 @@ const aboutUs = () => {
   <img src="${imageAlexis}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Alexis Arnaud</h5>
-    <button id="showStory"> description </button>
+    <div class="text-center">
+    <button id="showStory" class="btn btn-info AboutUsPage-description-button"> Description </button>
+    </div>
     <dialog id="Story">
       <form method="dialog">
         <p>
         Alexis est le responsable du backend. Il s'est chargé de la page de création de quizzes, la page de visionnage de tous les quizzes par catégorie, ainsi que la page d'affichage d'un quizz.
         </p>
-          <button id="confirmBtn">Close</button>
+        <div class="text-center">
+          <button id="confirmBtn" class="btn btn-dark" style="font-family:sans-serif">Close</button>
+        </div>
       </form>
     </dialog>
   </div>
@@ -41,11 +49,15 @@ const aboutUs = () => {
   <img src="${imageKeyvan}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Keyvan Nikazm</h5>
-    <button id="showStory"> description </button>
+    <div class="text-center">
+    <button id="showStory" class="btn btn-info AboutUsPage-description-button"> Description </button>
+    </div>
         <dialog id="Story">
           <form method="dialog">
             <p>Pudor in debeo sum nostro illum orationi fortius quidem fortius postulabat aetas fortius datam Neque robustioribus tuli patiebatur meum versari aliquis Atratino hunc accusationis accusationis erga dicendi et Vellem debeo enim Atratino dicendi tueri et accusationis patiebatur tuli parentemque istam tuus quidem et vobis accusationis accusationis moderatur beneficium in magis.</p>
-              <button id="confirmBtn">Close</button>
+            <div class="text-center">
+            <button id="confirmBtn" class="btn btn-dark" style="font-family:sans-serif">Close</button>
+          </div>
           </form>
         </dialog>
   </div>
@@ -54,14 +66,18 @@ const aboutUs = () => {
   <img src="${imageSimon}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Simon Bosseler</h5>
-      <button id="showStory"> description </button>
+    <div class="text-center">
+    <button id="showStory" class="btn btn-info AboutUsPage-description-button"> Description </button>
+    </div>
         <dialog id="Story">
           <form method="dialog">
             <p> Simon est l'un des deux conceptualisateurs de la base de données avec son acolyte Nicolas. Ils ont fourni une base de données structurées afin de déployer une application fonctionnelle et dans le cadre d'une expérience utilisateur à la hauteur des attentes.
             <br> Il a participé à l'insertion de toutes les données dans la base de données ainsi qu'au déploiement de celle-ci.
              <br> Il a également participé au backend de la création des quizzes.
             </p>
-              <button id="confirmBtn">Close</button>
+            <div class="text-center">
+            <button id="confirmBtn" class="btn btn-dark" style="font-family:sans-serif">Close</button>
+          </div>
           </form>
         </dialog>
 
@@ -71,7 +87,9 @@ const aboutUs = () => {
   <img src="${imageIsmet}" class="card-img-top" alt="...">
   <div class="card-body-AboutUs">
     <h5 class="card-title">Ismet Ismet</h5>
-    <button id="showStory"> description </button>
+    <div class="text-center">
+    <button id="showStory" class="btn btn-info AboutUsPage-description-button"> Description </button>
+    </div>
     <dialog id="Story">
       <form method="dialog">
         <p>
@@ -84,7 +102,9 @@ const aboutUs = () => {
            érigeant des remparts numériques impénétrables. Ismet, sculpteur d'expériences, 
            forge un cyberespace sécurisé où chaque clic résonne comme une symphonie technologique.
         </p>
-          <button id="confirmBtn">Close</button>
+        <div class="text-center">
+          <button id="confirmBtn" class="btn btn-dark" style="font-family:sans-serif">Close</button>
+        </div>
       </form>
     </dialog>
   </div>
